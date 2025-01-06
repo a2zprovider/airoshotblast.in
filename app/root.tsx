61277c326af5cd3375558f9f42a3f718a35d7aec
a2zproviders <a2zproviders95@gmail.com>
2025-01-06 18:02:33 +0530

    updates

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ import Layout from "./components/Layout";
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
-  { rel: "preload", href: "https://fonts.googleapis.com/css2?family=Solway:wght@300;400;500;700;800&display=swap" },
+  // { rel: "preload", href: "https://fonts.googleapis.com/css2?family=Solway:wght@300;400;500;700;800&display=swap" },
   { rel: 'stylesheet', href: '/fontawesome/css/all.min.css' },
 ];
 
@@ -171,7 +171,7 @@ export default function App() {
             }}
           />
           <script async src="https://www.google.com/recaptcha/api.js" ></script>
-          <link rel="preload" href="https://fonts.googleapis.com/css2?family=Solway:wght@300;400;500;700;800&display=swap" as="style" />
+          {/* <link rel="preload" href="https://fonts.googleapis.com/css2?family=Solway:wght@300;400;500;700;800&display=swap" as="style" /> */}
         </head>
         <body className="text-[#131B23]">
           <Header settings={settings?.data} />
