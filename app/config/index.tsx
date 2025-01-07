50ccc44c9a2c041953a19bff10edb01df95f5299
a2zproviders <a2zproviders95@gmail.com>
2025-01-07 16:01:01 +0530

    update

--- app/config/index.tsx
+++ app/config/index.tsx
@@ -9,7 +9,7 @@ const config = {
     // RECAPTCHA_SECRET_KEY: '6LdR7o8qAAAAAFlk6kO-tT5s_LOvBNt6CfOvAYVL',
     // RECAPTCHA_SITE_KEY: '6LdR7o8qAAAAAO__edju4RhYvsoSyZ9wiAQE-80u',
 
-
+    
     imgBaseURL: 'http://localhost:5000/upload/',    
     apiBaseURL: 'http://localhost:5000/api/',
     RECAPTCHA_SECRET_KEY: '6LdR7o8qAAAAAFlk6kO-tT5s_LOvBNt6CfOvAYVL',
