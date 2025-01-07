71c22e3b8d104d1e761ee01376952ee7b053ad25
a2zproviders <a2zproviders95@gmail.com>
2025-01-07 16:05:28 +0530

    up

--- app/config/index.tsx
+++ app/config/index.tsx
@@ -8,7 +8,6 @@ const config = {
     // apiBaseURL: 'http://185.166.39.93:5000/api/',
     // RECAPTCHA_SECRET_KEY: '6LdR7o8qAAAAAFlk6kO-tT5s_LOvBNt6CfOvAYVL',
     // RECAPTCHA_SITE_KEY: '6LdR7o8qAAAAAO__edju4RhYvsoSyZ9wiAQE-80u',
-
     
     imgBaseURL: 'http://localhost:5000/upload/',    
     apiBaseURL: 'http://localhost:5000/api/',
